c6fa44cf56416b086c7d0274adc057910d0888ed
Caroline Weinberger <caroline.weinberger04@gmail.com>
2025-07-01 12:46:47 +0200

    essai3

--- routes/travel.js
+++ routes/travel.js
@@ -5,24 +5,33 @@ const fetch = require('node-fetch');
 const Travel = require('../models/travel');
 
 
-router.get('/',(req,res) => {
-    Travel.find(departure === req.body.departure, arrival === req.body.arrival,  date === req.body.date, price === req.body.price)
-    .then (data => {
-             res.json({result : true, travel : newTravel});
+// router.get('/',(req,res) => {
+//     Travel.find()
+//     .then (data => {
+//         const Travel = ({
+//             departure : req.body.departure,
+//             arrival : req.body.arrival,
+//             date : req.body.date,
+//             price : req.body.price
+//         })
+//         res.json({result : true, travel : data});
+//     })
+// })
+
+
+
+
+
+router.post('/',(req,res) => {
+    Travel.find().then (data => {
+        res.json ({travel : data})
     })
-})
-
-
+});
 
 
 // router.get('/',(req,res) => {
-//     Travel.find().then (data => {
-//         res.json ({travel: data})
-//     })
+// res.json ({allTravel: travel});
 // });
-
-
-
 
 
 
