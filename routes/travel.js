f67b2d87db8e88617014d8ce0da52799f2dfea2c
Caroline Weinberger <caroline.weinberger04@gmail.com>
2025-07-01 14:58:19 +0200

    env

--- routes/travel.js
+++ routes/travel.js
@@ -4,29 +4,41 @@ var router = express.Router();
 const fetch = require('node-fetch');
 const Travel = require('../models/travel');
 
+router.post('/search',(req,res) => {
+    Travel.find({departure : req.body.departure, arrival: req.body.arrival}).then (data => {
+       
+        for (let index = 0; index < data.length; index++) {
 
-// router.get('/',(req,res) => {
-//     Travel.find()
-//     .then (data => {
-//         const Travel = ({
-//             departure : req.body.departure,
-//             arrival : req.body.arrival,
-//             date : req.body.date,
-//             price : req.body.price
-//         })
-//         res.json({result : true, travel : data});
-//     })
-// })
+           let date = new Date ();
+       let day = date.getDate();
+    if(day<10){
+        day = `0${day}`;
+        console.log(`0${day}`);
+    }
+let month = date.getMonth()+1;
+  if(month<10){
+        month = `0${month}`;
+        console.log(`0${month}`);
+    }
+let year = date.getFullYear();
+
+         const searchDate = `${year}-${month}-${day}`;
+           if(req.body.date === searchDate){
+                 res.json ({travel : data})
+        return
+        }}
+    });
+})
+    
 
 
 
 
 
-router.post('/',(req,res) => {
-    Travel.find().then (data => {
-        res.json ({travel : data})
-    })
-});
+
+
+
+
 
 
 // router.get('/',(req,res) => {
